2efd800521afd9f831ccb3fc492e967f03559277
SARUCA1 <santiagoruizc@gmail.com>
2022-03-07 20:31:07 -0600

    Creacion

--- L03_AES/BinaryList.cs
+++ L03_AES/BinaryList.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace L03_AES
 {
-    public class Class1
+    public class BinaryList
     {
+
     }
 }
---
+++ WebApp/Helpers/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using L03_AES;
+using WebApp.Models;
+
+namespace WebApp.Helpers
+{
+    public class Data
+    {
+        private static Data _instance = null;
+
+        //public BinaryList<SATModel> Lista = new BinaryList<SATModel>();
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+        public List<SATModel> ListaS = new List<SATModel>
+        {
+            
+        };
+    }
+}
